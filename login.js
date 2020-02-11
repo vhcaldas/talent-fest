92fdd4d00e39188bc080f1650a3b7f0e86404ab2
vhcaldas <vivianhcaldas@gmail.com>
2020-02-11 15:00:01 -0300

    captura de imagens

--- login.js
+++ login.js
@@ -3,15 +3,30 @@
 const btnCapture = document.getElementById('capture');
 const player = document.getElementById('player');
 const snapshotCanvas = document.getElementById('snapshot');
+let videoTracks;
+
 
 const handleSuccess = function (stream) {
 	player.srcObject = stream;
+	videoTracks = stream.getVideoTracks();
+
 };
 navigator.mediaDevices.getUserMedia({ video: true })
 	.then(handleSuccess);
 
 btnCapture.addEventListener("click", function () {
-	
-})
+	const context = snapshot.getContext('2d');
+	// Draw the video frame to the canvas.
+	context.drawImage(player, 0, 0, snapshotCanvas.width,
+		snapshotCanvas.height);
+	videoTracks.forEach(function(track) {track.stop()});
+
+});
+
+const addToFirebase = () => {
+	console.log('deu certo!')
+}
+
+
 
 
