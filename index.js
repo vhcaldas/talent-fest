f421a62388a7211723424f4f774c03843a16374c
Evs <evoraleite@gmail.com>
2020-02-11 12:42:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const btnLogin = document.getElementById('btn-login');
+const btnRegister = document.getElementById('btn-register');
+
+btnLogin.addEventListener("click", function() {
+	console.log('funciona')
+})
+
+btnRegister.addEventListener("click", function() {
+	console.log('funciona')
+})
