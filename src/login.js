fc34b98523e7a231c8a817489918941f9ee8b2a7
vhcaldas <vivianhcaldas@gmail.com>
2020-02-12 12:00:08 -0300

    Função detectFace implementada

--- src/login.js
+++ src/login.js
@@ -14,58 +14,47 @@ btnLogin.addEventListener("click", function () {
 
 
 btnCapture.addEventListener("click", function () {
-	const context = snapshot.getContext('2d');
-	// const gl = snapshotCanvas.getContext('webgl', {preserveDrawingBuffer: true})
-	context.drawImage(player, 0, 0, snapshotCanvas.width,
-		snapshotCanvas.height)
-	const canvas = snapshotCanvas.toDataURL();
-	//const pictureName = (`${new Date()}.png`);
-	const fbStorage = firebase.storage().ref();
-	const child = fbStorage.child('users/'+(new Date())+'.png')
+    const context = snapshot.getContext('2d');
+    // const gl = snapshotCanvas.getContext('webgl', {preserveDrawingBuffer: true})
+    context.drawImage(player, 0, 0, snapshotCanvas.width,
+        snapshotCanvas.height)
+    const canvas = snapshotCanvas.toDataURL();
+    //const pictureName = (`${new Date()}.png`);
+    const fbStorage = firebase.storage().ref();
+    const child = fbStorage.child('users/' + (new Date()) + '.png')
 
-	child.putString(canvas, 'data_url').then(snap => getImageFromFirebase(child))	
-	//videoTracks.forEach(function(track) {track.stop()});
+    child.putString(canvas, 'data_url').then(snap => getImageFromFirebase(child))
+    //videoTracks.forEach(function(track) {track.stop()});
 });
 
 const getImageFromFirebase = (img) => {
-	img.getDownloadURL().then(url => console.log(url))
+    img.getDownloadURL().then(url => detectFace(url))
 }
 
 const handleSuccess = function (stream) {
-	player.srcObject = stream;
-	//videoTracks = stream.getVideoTracks();
+    player.srcObject = stream;
+    //videoTracks = stream.getVideoTracks();
 };
 
 navigator.mediaDevices.getUserMedia({ video: true })
     .then(handleSuccess);
 
-function detectFace() {
-    const params = {
-        "returnFaceId": "true",
-        "returnFaceLandmarks": "false",
-        "returnFaceAttributes": "string",
-        "recognitionModel": "recognition_02",
-        "returnRecognitionModel": "false",
-        "detectionModel": "detection_02",
-    };
-
-    $.ajax({
-        url: "https://laboratoriaface.cognitiveservices.azure.com/face/v1.0/detect" + $.param(params),
-        beforeSend: function (xhrObj) {
-            // Request headers
-            xhrObj.setRequestHeader("Content-Type", "application/json");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a76225be0539425da6665eeb02a8973f");
+function detectFace(url) {
+    const settings = {
+        "url": "https://laboratoriaface.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_02&returnRecognitionModel=false&detectionModel=detection_02",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": "a76225be0539425da6665eeb02a8973f"
         },
-        type: "POST",
-        // Request body
-        data: { "url": "https://firebasestorage.googleapis.com/v0/b/talent-fest-2020.appspot.com/o/users%2F0.0103612480528672.png?alt=media&token=224bc37d-11e2-4073-a439-470d3d308412"},
-    })
-        .done(function (data) {
-            console.log("funcionou");
-        })
-        .fail(function () {
-            alert("error");
-        });
+        "data": JSON.stringify({ "url": `${url}` }),
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    }).fail(function () {
+        alert("error");
+    });
 };
 
 /* function processImage() {
