10764b111706825c08f589bd9912f615bafbfde1
vhcaldas <vivianhcaldas@gmail.com>
2020-02-12 14:19:52 -0300

    Finalizada função de busca de nome

--- src/login.js
+++ src/login.js
@@ -40,8 +40,6 @@ navigator.mediaDevices.getUserMedia({ video: true })
     .then(handleSuccess);
 
 function detectFace(url) {
-
-
     const settings = {
         "url": "https://laboratoriaface.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_01&returnRecognitionModel=false&detectionModel=detection_01",
         "method": "POST",
@@ -54,7 +52,6 @@ function detectFace(url) {
     };
     $.ajax(settings).done(function (response) {
         identifyImage(response);
-        console.log(response);
     }).fail(function () {
         alert("error");
     });
@@ -63,13 +60,13 @@ function detectFace(url) {
 function identifyImage(response) {
     const faceId = response[0].faceId;
 
-    var myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Ocp-Apim-Subscription-Key", "a76225be0539425da6665eeb02a8973f");
 
-    var raw = JSON.stringify({ "personGroupId": "laboratoria", "faceIds": [faceId], "maxNumOfCandidatesReturned": 1, "confidenceThreshold": 0.5 });
+    const raw = JSON.stringify({ "personGroupId": "laboratoria", "faceIds": [faceId], "maxNumOfCandidatesReturned": 1, "confidenceThreshold": 0.5 });
 
-    var requestOptions = {
+    const requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
@@ -78,7 +75,27 @@ function identifyImage(response) {
 
     fetch("https://laboratoriaface.cognitiveservices.azure.com/face/v1.0/identify", requestOptions)
         .then(response => response.text())
-        .then(result => console.log(result))
+        .then(result => getName(result))
         .catch(error => console.log('error', error));
-
 };
+
+function getName(result) {
+    const settings = {
+        "url": "https://laboratoriaface.cognitiveservices.azure.com/face/v1.0/persongroups/laboratoria/persons/8200e14b-d890-4286-a3be-b669a3fc8196",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Ocp-Apim-Subscription-Key": "a76225be0539425da6665eeb02a8973f",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "{body}": ""
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response.name);
+    }).fail(error => alert('error', error));
+}
+
+
